e68614c122a7ece6122ca1c245c24b359a2943cf
ltpoole <laborris.poole@gmail.com>
2023-10-17 22:40:55 -0400

    Completed RProblemTwo

--- LINQLab/Problems.cs
+++ LINQLab/Problems.cs
@@ -21,7 +21,7 @@ namespace LINQLab
             //RDemoOne();
             //RProblemOne();
             RDemoTwo();
-            //RProblemTwo();
+            RProblemTwo();
             //RProblemThree();
             //RProblemFour();
             //RProblemFive();
@@ -97,7 +97,12 @@ namespace LINQLab
         {
             // Write a LINQ query that gets each product whose price is less than or equal to $100.
             // Print the name and price of all products
-
+            var productsUnder100 = _context.Products.Where(p => p.Price <= 100);
+            Console.WriteLine("Products less than or equal to $100");
+            foreach (Product product in productsUnder100)
+            {
+                Console.WriteLine($"{product.Name} - ${product.Price}");
+            }
         }
 
         /*
